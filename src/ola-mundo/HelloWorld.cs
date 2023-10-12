142dfa08124f37a9cb0a3127b3d4afd47dbde905
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-10-12 19:40:01 -0300

    feat(HelloWorld.cs): Retornar string "Olá, Mundo!" no método "Greet"

--- src/ola-mundo/HelloWorld.cs
+++ src/ola-mundo/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld
     // 1 - Retorne a string 'Olá, Mundo!'
     public static string Greet()
     {
-        throw new NotImplementedException();
+        return "Olá, Mundo!";
     }
     // 2 - Retorne a string 'Seja bem-vindo, Tryber!
     public static string GreetTryber()
